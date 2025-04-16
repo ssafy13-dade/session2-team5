babd5a0e83dc0025c2ebaa4ba11ffe3a56caad0c
유건우 <dbrjsdn1220@gmail.com>
2025-04-16 13:15:39 +0900

    파일명이 kafka이면 kafka 모듈 작동 안해서 변경, 테스트로 메시지 소비하는 부분도 추가

--- data-engineering/kafka_producer.py
+++ data-engineering/kafka_producer.py
@@ -1,6 +1,6 @@
 import csv
 import json
-from kafka import KafkaProducer
+from kafka import KafkaConsumer, KafkaProducer
 
 # Kafka Producer 생성
 producer = KafkaProducer(
@@ -8,7 +8,10 @@ producer = KafkaProducer(
     value_serializer=lambda v: json.dumps(v).encode('utf-8')  # 메시지 직렬화 방식 설정
 )
 
+# 파일은 처리하지 않은 파일을 순서대로 처리할 수 있게 선택하는 방법을 강구할 것
 file_path = "../raw_data/train/1000_chg.csv"
+
+# Kafka로 send
 with open(file_path, newline='', encoding='utf-8') as file:
     data = csv.reader(file)
     headers = next(data)
@@ -34,3 +37,17 @@ with open(file_path, newline='', encoding='utf-8') as file:
         for i in range(199, 231):
             BMT[headers[i]] = row[i]
         producer.send('bmt_data', value=BMT)
+
+# Test Kafka Consumer 생성
+consumer = KafkaConsumer(
+	'bms_data',   # 구독할 토픽 설정 
+    'bcv_data',
+    'bmt_data',
+    bootstrap_servers="localhost:9092",   # Kafka 브로커 주소 설정
+    auto_offset_reset="earliest",   # 오프셋 초기화 방식 설정
+    enable_auto_commit=True,   # 자동 오프셋 커밋 여부 설정
+)
+
+# json.loads 써서 decode한 내용을 바로 key: value로 접근 가능(안하면 문자열 형태)
+for message in consumer:
+    print(f"[{message.topic}] {message.value.decode('utf-8')}")
